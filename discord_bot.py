e31999e30d168627542f0ca3f776e55f1715d239
Dmitriy <thebrootos@gmail.com>
2021-08-21 19:57:48 +0500

    first commit 21.08.2021

---
+++ discord_bot.py
@@ -0,0 +1,23 @@
+import discord
+
+from codes import token
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        my_list = ['Коля', 'Саня', 'Дима']
+
+        if message.content in my_list or message.content:
+            await message.channel.send(
+                f'Возврат сообщения: {message.content}')
+
+
+client = MyClient()
+client.run(token)
