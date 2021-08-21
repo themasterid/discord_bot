c8640083c01d7f5c2e4da01d0f5807f77b478d8c
Dmitriy <thebrootos@gmail.com>
2021-08-21 20:00:04 +0500

    first commit 21.08.2021

--- discord_bot.py
+++ discord_bot.py
@@ -8,7 +8,6 @@ class MyClient(discord.Client):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        # don't respond to ourselves
         if message.author == self.user:
             return
 
